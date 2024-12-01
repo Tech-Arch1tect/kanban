836b1194a891204f624389a7f16831b331bec8e9
tech-architect <108552582+Tech-Arch1tect@users.noreply.github.com>
2024-12-01 20:09:16 +0000

    [client] add api client (openapi-generator-cli generate --skip-validate-spec -i ..\..\server\docs\swagger.yaml -g typescript-fetch -o typescript-fetch-client)

---
+++ client/src/typescript-fetch-client/models/index.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdminControllerListUsersResponse';
+export * from './AdminControllerRemoveUserResponse';
+export * from './AdminControllerUpdateUserRoleRequest';
+export * from './AdminControllerUpdateUserRoleResponse';
+export * from './AuthControllerChangePasswordRequest';
+export * from './AuthControllerChangePasswordResponse';
+export * from './AuthControllerConfirmTOTPRequest';
+export * from './AuthControllerConfirmTOTPResponse';
+export * from './AuthControllerDisableTOTPRequest';
+export * from './AuthControllerDisableTOTPResponse';
+export * from './AuthControllerEnableTOTPRequest';
+export * from './AuthControllerEnableTOTPResponse';
+export * from './AuthControllerGenerateTOTPResponse';
+export * from './AuthControllerLoginRequest';
+export * from './AuthControllerLoginResponse';
+export * from './AuthControllerPasswordResetRequest';
+export * from './AuthControllerPasswordResetResponse';
+export * from './AuthControllerRegisterRequest';
+export * from './AuthControllerResetPasswordRequest';
+export * from './AuthControllerResetPasswordResponse';
+export * from './GormDeletedAt';
+export * from './MiscControllerAppNameResponse';
+export * from './ModelsErrorResponse';
+export * from './ModelsRole';
+export * from './ModelsUser';
