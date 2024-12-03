641b9e3f4ab3cd0da8c6ddd5cacb5cfc6ed29bde
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2024-12-03 20:52:18 +0000

    [client] Update fetchapi to grab CSRF token if request is post, put, delete or patch

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -4,17 +4,47 @@ import {
   AuthApi,
   AdminApi,
   MiscApi,
+  AuthControllerGetCSRFTokenResponse,
 } from "../typescript-fetch-client";
 
 // @ts-ignore
 const basePath = import.meta.env.VITE_API_BASE_PATH || "http://localhost:8090";
 
+
 const configuration = new Configuration({
   basePath: basePath,
   credentials: "include",
+  fetchApi: csrfFetch,
 });
 
 export const api = new BaseAPI(configuration);
 export const authApi = new AuthApi(configuration);
 export const adminApi = new AdminApi(configuration);
 export const miscApi = new MiscApi(configuration);
+
+async function GetCSRFToken(): Promise<string | undefined> {
+  const response: AuthControllerGetCSRFTokenResponse = await authApi.apiV1AuthCsrfTokenGet();
+  return response.csrfToken;
+}
+
+async function csrfFetch(input: RequestInfo, init?: RequestInit): Promise<Response> {
+  const methodsRequiringCSRF = ["POST", "PUT", "DELETE", "PATCH"];
+  const method = (init?.method || "GET").toUpperCase();
+
+  if (methodsRequiringCSRF.includes(method)) {
+    const csrfToken = await GetCSRFToken();
+    if (csrfToken) {
+      init = {
+        ...init,
+        headers: {
+          ...(init?.headers || {}),
+          "X-CSRF-Token": csrfToken,
+        },
+      };
+    } else {
+      throw new Error("Unable to obtain CSRF token.");
+    }
+  }
+
+  return fetch(input, init);
+}
