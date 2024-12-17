a33c9d5d6419d3c4a1ba58922e235983e162a5ff
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2024-12-17 20:18:55 +0000

    [front] useCreateTask: invalidate tasks query when creating a task

--- client/src/hooks/tasks/useCreateTask.ts
+++ client/src/hooks/tasks/useCreateTask.ts
@@ -1,20 +1,26 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { tasksApi } from "../../lib/api";
 import { TaskControllerCreateTaskRequest } from "../../typescript-fetch-client/models";
 
 export const useCreateTask = () => {
+  const queryClient = useQueryClient();
+
   const { mutate, error, isError, isSuccess, data, isPending } = useMutation({
     mutationFn: async (task: TaskControllerCreateTaskRequest) => {
-      return await tasksApi.apiV1TasksCreatePost({
-        request: {
-          boardId: task.boardId,
-          title: task.title,
-          description: task.description,
-          status: task.status,
-          swimlaneId: task.swimlaneId,
-          columnId: task.columnId,
-        },
-      });
+      return await tasksApi
+        .apiV1TasksCreatePost({
+          request: {
+            boardId: task.boardId,
+            title: task.title,
+            description: task.description,
+            status: task.status,
+            swimlaneId: task.swimlaneId,
+            columnId: task.columnId,
+          },
+        })
+        .then(() => {
+          queryClient.invalidateQueries({ queryKey: ["tasks"] });
+        });
     },
   });
 
