1cab5565b7132037e484cff9bba1521a0916d458
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-01-03 20:59:05 +0000

    [client] add more routes which are excluded from CSRF (password reset)
    * this is necessary currently as CSRF tokens are tied to user accounts. However in the future these will be decoupled and CSRF tokens will be managed purely in the session, logged in or not.

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -4,12 +4,11 @@ import {
   AuthApi,
   AdminApi,
   MiscApi,
-  AuthControllerGetCSRFTokenResponse,
+  AuthAuthGetCSRFTokenResponse,
 } from "../typescript-fetch-client";
 
 // @ts-ignore
 const basePath = import.meta.env.VITE_API_BASE_PATH || "http://localhost:8090";
-
 
 const configuration = new Configuration({
   basePath: basePath,
@@ -23,15 +22,26 @@ export const adminApi = new AdminApi(configuration);
 export const miscApi = new MiscApi(configuration);
 
 async function GetCSRFToken(): Promise<string | undefined> {
-  const response: AuthControllerGetCSRFTokenResponse = await authApi.apiV1AuthCsrfTokenGet();
+  const response: AuthAuthGetCSRFTokenResponse =
+    await authApi.apiV1AuthCsrfTokenGet();
   return response.csrfToken;
 }
 
-async function csrfFetch(input: RequestInfo, init?: RequestInit): Promise<Response> {
+async function csrfFetch(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<Response> {
   const methodsRequiringCSRF = ["POST", "PUT", "DELETE", "PATCH"];
   const method = (init?.method || "GET").toUpperCase();
 
-  if (methodsRequiringCSRF.includes(method) && !input.toString().includes("api/v1/auth/register") && !input.toString().includes("api/v1/auth/login") && !input.toString().includes("api/v1/auth/totp/confirm")) {
+  if (
+    methodsRequiringCSRF.includes(method) &&
+    !input.toString().includes("api/v1/auth/register") &&
+    !input.toString().includes("api/v1/auth/login") &&
+    !input.toString().includes("api/v1/auth/totp/confirm") &&
+    !input.toString().includes("api/v1/auth/password-reset") &&
+    !input.toString().includes("api/v1/auth/reset-password")
+  ) {
     const csrfToken = await GetCSRFToken();
     if (csrfToken) {
       init = {
