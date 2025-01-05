d9fa2c8bb99c1c94436174aa6deb0ac30a9ba647
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-01-05 18:52:39 +0000

    [client] add react-toastify

--- client/src/components/layout/navbar.tsx
+++ client/src/components/layout/navbar.tsx
@@ -4,6 +4,7 @@ import { useUserProfile } from "../../hooks/useUserProfile";
 import { useAuth } from "../../hooks/auth/useAuth";
 import { useDropdown } from "../../hooks/useDropdown";
 import BoardsSelect from "./BoardsSelect";
+import { ToastContainer } from "react-toastify";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -31,80 +32,38 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="bg-blue-800 shadow-lg">
-      <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-        <div className="flex items-center space-x-4">
-          <Link
-            to="/"
-            activeProps={{ className: "text-white font-bold" }}
-            inactiveProps={{ className: "text-gray-200 hover:text-white" }}
-            className="text-lg"
-          >
-            Home
-          </Link>
-          <Link
-            to="/about"
-            activeProps={{ className: "text-white font-bold" }}
-            inactiveProps={{ className: "text-gray-200 hover:text-white" }}
-            className="text-lg"
-          >
-            About
-          </Link>
-        </div>
-        <div className="flex items-center space-x-4">
-          <BoardsSelect />
-        </div>
-        <div className="flex items-center space-x-4">
-          {/* User Profile Dropdown */}
-          <div className="relative" ref={profileDropdown.ref}>
-            <button
-              onClick={profileDropdown.toggleDropdown}
-              className="text-white text-lg font-medium flex items-center space-x-2"
+    <>
+      <nav className="bg-blue-800 shadow-lg">
+        <div className="container mx-auto px-6 py-3 flex justify-between items-center">
+          <div className="flex items-center space-x-4">
+            <Link
+              to="/"
+              activeProps={{ className: "text-white font-bold" }}
+              inactiveProps={{ className: "text-gray-200 hover:text-white" }}
+              className="text-lg"
             >
-              Settings
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-            {profileDropdown.isOpen && (
-              <div className="dropdown absolute right-0 mt-2 w-48 bg-white shadow-xl rounded">
-                <Link
-                  to="/profile/profile"
-                  onClick={profileDropdown.closeDropdown}
-                  className="dropdown-item block px-4 py-2 hover:bg-gray-100"
-                >
-                  Profile
-                </Link>
-                <Link
-                  to="/profile/2fa"
-                  onClick={profileDropdown.closeDropdown}
-                  className="dropdown-item block px-4 py-2 hover:bg-gray-100"
-                >
-                  Manage 2FA
-                </Link>
-              </div>
-            )}
+              Home
+            </Link>
+            <Link
+              to="/about"
+              activeProps={{ className: "text-white font-bold" }}
+              inactiveProps={{ className: "text-gray-200 hover:text-white" }}
+              className="text-lg"
+            >
+              About
+            </Link>
           </div>
-
-          {/* Admin Dropdown */}
-          {isAdmin && (
-            <div className="relative" ref={adminDropdown.ref}>
+          <div className="flex items-center space-x-4">
+            <BoardsSelect />
+          </div>
+          <div className="flex items-center space-x-4">
+            {/* User Profile Dropdown */}
+            <div className="relative" ref={profileDropdown.ref}>
               <button
-                onClick={adminDropdown.toggleDropdown}
+                onClick={profileDropdown.toggleDropdown}
                 className="text-white text-lg font-medium flex items-center space-x-2"
               >
-                Admin
+                Settings
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-4 w-4"
@@ -120,29 +79,74 @@ const Navbar = () => {
                   />
                 </svg>
               </button>
-              {adminDropdown.isOpen && (
+              {profileDropdown.isOpen && (
                 <div className="dropdown absolute right-0 mt-2 w-48 bg-white shadow-xl rounded">
                   <Link
-                    to="/admin/users"
-                    onClick={adminDropdown.closeDropdown}
+                    to="/profile/profile"
+                    onClick={profileDropdown.closeDropdown}
                     className="dropdown-item block px-4 py-2 hover:bg-gray-100"
                   >
-                    Users
+                    Profile
+                  </Link>
+                  <Link
+                    to="/profile/2fa"
+                    onClick={profileDropdown.closeDropdown}
+                    className="dropdown-item block px-4 py-2 hover:bg-gray-100"
+                  >
+                    Manage 2FA
                   </Link>
                 </div>
               )}
             </div>
-          )}
 
-          <button
-            onClick={handleLogout}
-            className="text-white text-lg font-medium hover:text-gray-200"
-          >
-            Logout
-          </button>
+            {/* Admin Dropdown */}
+            {isAdmin && (
+              <div className="relative" ref={adminDropdown.ref}>
+                <button
+                  onClick={adminDropdown.toggleDropdown}
+                  className="text-white text-lg font-medium flex items-center space-x-2"
+                >
+                  Admin
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M19 9l-7 7-7-7"
+                    />
+                  </svg>
+                </button>
+                {adminDropdown.isOpen && (
+                  <div className="dropdown absolute right-0 mt-2 w-48 bg-white shadow-xl rounded">
+                    <Link
+                      to="/admin/users"
+                      onClick={adminDropdown.closeDropdown}
+                      className="dropdown-item block px-4 py-2 hover:bg-gray-100"
+                    >
+                      Users
+                    </Link>
+                  </div>
+                )}
+              </div>
+            )}
+
+            <button
+              onClick={handleLogout}
+              className="text-white text-lg font-medium hover:text-gray-200"
+            >
+              Logout
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <ToastContainer position="bottom-right" newestOnTop theme="colored" />
+    </>
   );
 };
 
