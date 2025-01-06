bf682908c0116899a4180222a92f8f458d1d7809
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-01-06 21:24:37 +0000

    [client] Add hooks for getting a task's data and editing a task

---
+++ client/src/hooks/tasks/useEditTask.ts
@@ -0,0 +1,35 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { tasksApi } from "../../lib/api";
+import { toast } from "react-toastify";
+import { TaskEditTaskRequest } from "../../typescript-fetch-client";
+
+export const useEditTask = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, error, isError, isSuccess, data, isPending } = useMutation({
+    mutationFn: async (task: TaskEditTaskRequest) => {
+      return await tasksApi
+        .apiV1TasksEditPost({
+          request: {
+            id: task.id,
+            title: task.title,
+            description: task.description,
+            status: task.status,
+            assigneeId: task.assigneeId,
+          },
+        })
+        .then(() => {
+          queryClient.invalidateQueries({ queryKey: ["tasks"] });
+          queryClient.invalidateQueries({ queryKey: ["task", task.id] });
+        });
+    },
+    onSuccess: () => {
+      toast.success("Task updated successfully!");
+    },
+    onError: (error: any) => {
+      toast.error(error.message || "Failed to update task.");
+    },
+  });
+
+  return { mutate, error, isError, isSuccess, data, isPending };
+};
