c4d5f99a11425c8d4027d7ac950238ca57e01394
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-01-15 23:09:45 +0000

    [client] add hooks to create/delete task links

---
+++ client/src/hooks/tasks/useCreateTaskLink.ts
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { tasksApi } from "../../lib/api";
+import { TaskCreateTaskLinkRequest } from "../../typescript-fetch-client";
+import { toast } from "react-toastify";
+
+export const useCreateTaskLink = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, error, isError, isSuccess, data, isPending } = useMutation({
+    mutationFn: async (link: TaskCreateTaskLinkRequest) => {
+      return await tasksApi
+        .apiV1TasksCreateLinkPost({
+          request: {
+            srcTaskId: link.srcTaskId,
+            dstTaskId: link.dstTaskId,
+            linkType: link.linkType,
+          },
+        })
+        .then(() => {
+          queryClient.invalidateQueries({ queryKey: ["tasks"] });
+        });
+    },
+    onSuccess: () => {
+      toast.success("Task link created successfully!");
+    },
+    onError: (error: any) => {
+      toast.error(error.message || "Failed to create task link.");
+    },
+  });
+
+  return { mutate, error, isError, isSuccess, data, isPending };
+};
---
+++ client/src/hooks/tasks/useDeleteTaskLink.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { tasksApi } from "../../lib/api";
+import { TaskDeleteTaskLinkRequest } from "../../typescript-fetch-client";
+import { toast } from "react-toastify";
+
+export const useDeleteTaskLink = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, error, isError, isSuccess, data, isPending } = useMutation({
+    mutationFn: async (link: TaskDeleteTaskLinkRequest) => {
+      return await tasksApi
+        .apiV1TasksDeleteLinkPost({
+          request: {
+            linkId: link.linkId,
+          },
+        })
+        .then(() => {
+          queryClient.invalidateQueries({ queryKey: ["tasks"] });
+        });
+    },
+    onSuccess: () => {
+      toast.success("Task link deleted successfully!");
+    },
+    onError: (error: any) => {
+      toast.error(error.message || "Failed to delete task link.");
+    },
+  });
+
+  return { mutate, error, isError, isSuccess, data, isPending };
+};
