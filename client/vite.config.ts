a32677245f74fb9988b315dd8aadb0f912a151b7
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-01-28 17:59:33 +0000

    [client] add react compiler beta

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
+const ReactCompilerConfig = {
+  target: '18'
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({}), react()],
+  plugins: [
+    TanStackRouterVite({}), 
+    react({
+      babel: {
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig],
+        ],
+      },
+    }),
+  ],
 })
