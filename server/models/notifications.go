30f45223cdb80fab4e5f7080ecb583e0eedabfb7
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-03 21:38:09 +0000

    [server] add json attributes to notification structs

--- server/models/notifications.go
+++ server/models/notifications.go
@@ -4,26 +4,28 @@ import "time"
 
 type NotificationConfiguration struct {
 	Model
-	UserID     uint                `gorm:"not null"`
-	User       User                `gorm:"foreignKey:UserID"`
-	Method     string              `gorm:"not null"` // e.g. "email", "webhook"
-	WebhookURL string              // required if Method=="webhook"
-	Email      string              // required if Method=="email"
-	Events     []NotificationEvent `gorm:"many2many:notification_configuration_events;"`
+	Name       string              `gorm:"not null" json:"name"`
+	UserID     uint                `gorm:"not null" json:"user_id"`
+	User       User                `gorm:"foreignKey:UserID" json:"user"`
+	Method     string              `gorm:"not null" json:"method"` // e.g. "email", "webhook"
+	WebhookURL string              `json:"webhook_url"`            // required if Method=="webhook"
+	Email      string              `json:"email"`                  // required if Method=="email"
+	Events     []NotificationEvent `gorm:"many2many:notification_configuration_events;" json:"events"`
+	Boards     []Board             `gorm:"many2many:notification_configuration_boards;" json:"boards"`
 }
 
 type NotificationEvent struct {
 	Model
-	Name                       string                      `gorm:"unique;not null"`
-	NotificationConfigurations []NotificationConfiguration `gorm:"many2many:notification_configuration_events;"`
+	Name                       string                      `gorm:"unique;not null" json:"name"`
+	NotificationConfigurations []NotificationConfiguration `gorm:"many2many:notification_configuration_events;" json:"notification_configurations"`
 }
 
 type NotificationLog struct {
 	Model
-	ConfigID    uint                      `gorm:"not null"`
-	Config      NotificationConfiguration `gorm:"foreignKey:ConfigID"`
-	EventName   string                    `gorm:"not null"`
-	Title       string                    `gorm:"not null"`
-	Description string                    `gorm:"not null"`
-	SentAt      time.Time
+	ConfigID    uint                      `gorm:"not null" json:"config_id"`
+	Config      NotificationConfiguration `gorm:"foreignKey:ConfigID" json:"config"`
+	EventName   string                    `gorm:"not null" json:"event_name"`
+	Title       string                    `gorm:"not null" json:"title"`
+	Description string                    `gorm:"not null" json:"description"`
+	SentAt      time.Time                 `json:"sent_at"`
 }
