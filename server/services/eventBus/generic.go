c97d6db07094db960055e496a905c5d6c5f44a54
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-06 18:15:33 +0000

    [server] generic event bus: Add global handlers
    Global handlers can subscript to all events

--- server/services/eventBus/generic.go
+++ server/services/eventBus/generic.go
@@ -1,17 +1,26 @@
 package eventBus
 
-type Handler[T any] func(event T)
+type Handler[T any] func(data T)
+type GlobalHandler[T any] func(event string, data T)
 
 type EventBus[T any] struct {
-	handlers map[string][]Handler[T]
+	handlers       map[string][]Handler[T]
+	globalHandlers []GlobalHandler[T]
 }
 
 func NewEventBus[T any]() *EventBus[T] {
-	return &EventBus[T]{handlers: make(map[string][]Handler[T])}
+	return &EventBus[T]{
+		handlers:       make(map[string][]Handler[T]),
+		globalHandlers: []GlobalHandler[T]{},
+	}
 }
 
 func (eb *EventBus[T]) Subscribe(event string, handler Handler[T]) {
 	eb.handlers[event] = append(eb.handlers[event], handler)
+}
+
+func (eb *EventBus[T]) SubscribeGlobal(handler GlobalHandler[T]) {
+	eb.globalHandlers = append(eb.globalHandlers, handler)
 }
 
 func (eb *EventBus[T]) Publish(event string, data T) {
@@ -19,5 +28,8 @@ func (eb *EventBus[T]) Publish(event string, data T) {
 		for _, handler := range handlers {
 			handler(data)
 		}
+	}
+	for _, handler := range eb.globalHandlers {
+		handler(event, data)
 	}
 }
