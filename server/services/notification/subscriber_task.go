0b65951826bc29d390d6e418bd0ba99e84f9727e
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-07 20:27:13 +0000

    [server] improve task notifications

--- server/services/notification/subscriber_task.go
+++ server/services/notification/subscriber_task.go
@@ -10,54 +10,87 @@ import (
 )
 
 func (ns *NotificationSubscriber) HandleTaskEvent(event string, task models.Task, user models.User) {
-	sendErr := ns.SendTaskNotifications(task, event, user)
-	if sendErr != nil {
-		log.Println("Error sending task notifications:", sendErr)
+	err := ns.SendTaskNotifications(task, event, user)
+	if err != nil {
+		log.Println("Error sending task notifications:", err)
 	}
 }
 
 func (ns *NotificationSubscriber) GetTaskGenericTemplate(event string, task models.Task, user models.User) (string, template.HTML) {
-	body := ""
+	var body string
+
 	if task.Board.Name != "" {
-		body += "Board: " + task.Board.Name + "<br>"
+		body += "<p><strong>Board:</strong> " + task.Board.Name + "</p>"
 	}
-	body += "@" + user.Username + " "
+	body += "User <strong>@" + user.Username + "</strong> "
+
 	switch event {
 	case "task.created":
 		subject := fmt.Sprintf("New Task Created: %s", task.Title)
 		body += "has created a new task with the following details:<br>"
-		body += fmt.Sprintf("<strong>Title:</strong> %s<br><strong>Description:</strong> %s<br><strong>Status:</strong> %s", task.Title, task.Description, task.Status)
+		body += fmt.Sprintf(
+			"<p><strong>Title:</strong> %s</p>"+
+				"<p><strong>Description:</strong></p><blockquote>%s</blockquote>"+
+				"<p><strong>Status:</strong> %s</p>",
+			task.Title, task.Description, task.Status,
+		)
 		return subject, template.HTML(body)
 
 	case "task.updated.title":
 		subject := "Task Updated: Title Modified"
-		body += fmt.Sprintf("has updated the title of task (ID: %d).<br><strong>New Title:</strong> %s", task.ID, task.Title)
+		body += fmt.Sprintf(
+			"has updated the title of task (ID: %d).<br><p><strong>New Title:</strong> %s</p>",
+			task.ID, task.Title,
+		)
 		return subject, template.HTML(body)
 
 	case "task.updated.description":
 		subject := "Task Updated: Description Modified"
-		body += fmt.Sprintf("has updated the description of task (ID: %d).<br><strong>Title:</strong> %s<br><strong>New Description:</strong> %s", task.ID, task.Title, task.Description)
+		body += fmt.Sprintf(
+			"has updated the description of task (ID: %d).<br><p><strong>Title:</strong> %s</p>"+
+				"<p><strong>New Description:</strong></p><blockquote>%s</blockquote>",
+			task.ID, task.Title, task.Description,
+		)
 		return subject, template.HTML(body)
 
 	case "task.updated.status":
 		subject := "Task Updated: Status Modified"
-		body += fmt.Sprintf("has updated the status of task (ID: %d).<br><strong>Title:</strong> %s<br><strong>New Status:</strong> %s", task.ID, task.Title, task.Status)
+		body += fmt.Sprintf(
+			"has updated the status of task (ID: %d).<br><p><strong>Title:</strong> %s</p>"+
+				"<p><strong>New Status:</strong> %s</p>",
+			task.ID, task.Title, task.Status,
+		)
 		return subject, template.HTML(body)
 
 	case "task.updated.assignee":
 		subject := "Task Updated: Assignee Modified"
-		body += fmt.Sprintf("has updated the assignee of task (ID: %d).<br><strong>Title:</strong> %s<br><strong>New Assignee:</strong> %s", task.ID, task.Title, task.Assignee.Username)
+		body += fmt.Sprintf(
+			"has updated the assignee of task (ID: %d).<br><p><strong>Title:</strong> %s</p>"+
+				"<p><strong>New Assignee:</strong> %s</p>",
+			task.ID, task.Title, task.Assignee.Username,
+		)
 		return subject, template.HTML(body)
 
 	case "task.deleted":
 		subject := fmt.Sprintf("Task Deleted: %s", task.Title)
 		body += "has deleted the following task:<br><br>"
-		body += fmt.Sprintf("<strong>Title:</strong> %s<br><strong>Description:</strong> %s", task.Title, task.Description)
+		body += fmt.Sprintf(
+			"<p><strong>Title:</strong> %s</p>"+
+				"<p><strong>Description:</strong></p><blockquote>%s</blockquote>",
+			task.Title, task.Description,
+		)
 		return subject, template.HTML(body)
 
 	case "task.moved":
 		subject := fmt.Sprintf("Task Moved: %s", task.Title)
-		body += fmt.Sprintf("has moved task (ID: %d) to a new location.<br><br><strong>Title:</strong> %s<br><strong>New Board:</strong> %s<br><strong>New Column:</strong> %s<br><strong>New Swimlane:</strong> %s", task.ID, task.Title, task.Board.Name, task.Column.Name, task.Swimlane.Name)
+		body += fmt.Sprintf(
+			"has moved task (ID: %d) to a new location.<br><br>"+
+				"<p><strong>Title:</strong> %s</p>"+
+				"<p><strong>New Board:</strong> %s</p>"+
+				"<p><strong>New Column:</strong> %s</p>"+
+				"<p><strong>New Swimlane:</strong> %s</p>",
+			task.ID, task.Title, task.Board.Name, task.Column.Name, task.Swimlane.Name,
+		)
 		return subject, template.HTML(body)
 
 	default:
@@ -77,7 +110,7 @@ func (ns *NotificationSubscriber) GatherTaskNotificationConfigurations(task mode
 }
 
 func (ns *NotificationSubscriber) SendTaskNotifications(task models.Task, event string, user models.User) error {
-	errRes := make([]error, 0)
+	var errRes []error
 	configs, err := ns.GatherTaskNotificationConfigurations(task, event)
 	if err != nil {
 		log.Println("Error gathering task notification configurations:", err)
@@ -88,10 +121,8 @@ func (ns *NotificationSubscriber) SendTaskNotifications(task models.Task, event 
 		return nil
 	}
 	for _, config := range configs {
-		if config.OnlyAssignee {
-			if config.UserID != task.AssigneeID {
-				continue
-			}
+		if config.OnlyAssignee && config.UserID != task.AssigneeID {
+			continue
 		}
 
 		subject, body := ns.GetTaskGenericTemplate(event, task, user)
@@ -104,7 +135,6 @@ func (ns *NotificationSubscriber) SendTaskNotifications(task models.Task, event 
 		}
 		err := ns.SendNotification(event, subject, tmplData, config)
 		if err != nil {
-			// dont stop sending notifications just because of one failed one
 			log.Println("Error sending notification:", err)
 			errRes = append(errRes, err)
 		}
