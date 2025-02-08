9f9ee51b36f329b985f65a17116e7a5c0971c3d7
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-08 20:11:39 +0000

    [client] Move friendlyEventNames to it's own file for re-use

--- client/src/components/profile/notifs/NotificationForm.tsx
+++ client/src/components/profile/notifs/NotificationForm.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useEventData } from "../../../hooks/notifications/useEventData";
 import { useCreateNotificationConfiguration } from "../../../hooks/notifications/useCreateNotificationConfiguration";
 import { useBoards } from "../../../hooks/boards/useBoards";
+import { friendlyEventNames } from "./friendlyEventNames";
 
 export const NotificationForm = () => {
   const [name, setName] = useState("");
@@ -55,27 +56,6 @@ export const NotificationForm = () => {
     };
 
     createNotification(payload);
-  };
-
-  const friendlyEventNames: Record<string, string> = {
-    "task.created": "Task created",
-    "task.updated.title": "Task title updated",
-    "task.updated.description": "Task description updated",
-    "task.updated.status": "Task status updated",
-    "task.updated.assignee": "Task assignee updated",
-    "task.deleted": "Task deleted",
-    "task.moved": "Task moved",
-    "comment.created": "Comment created",
-    "comment.updated": "Comment updated",
-    "comment.deleted": "Comment deleted",
-    "file.created": "File created",
-    "file.updated": "File updated",
-    "file.deleted": "File deleted",
-    "link.created": "Link created",
-    "link.deleted": "Link deleted",
-    "externallink.created": "External link created",
-    "externallink.updated": "External link updated",
-    "externallink.deleted": "External link deleted",
   };
 
   return (
