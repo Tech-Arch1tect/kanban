e9890f0fad481857bf30e565d7d39c82bc120f9b
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-08 19:50:45 +0000

    [server] NotificationConfigurationRepository: Add UpdateAndReplaceEventsAndBoards func

--- server/database/repository/notificationConfiguration _repository.go
+++ server/database/repository/notificationConfiguration _repository.go
@@ -8,6 +8,7 @@ import (
 
 type NotificationConfigurationRepository interface {
 	Repository[models.NotificationConfiguration]
+	UpdateAndReplaceEventsAndBoards(notification *models.NotificationConfiguration) error
 }
 
 type GormNotificationConfigurationRepository struct {
@@ -22,4 +23,20 @@ func NewNotificationConfigurationRepository(db *gorm.DB) NotificationConfigurati
 
 func (r *GormNotificationConfigurationRepository) Migrate() error {
 	return r.db.AutoMigrate(&models.NotificationConfiguration{})
+}
+
+func (r *GormNotificationConfigurationRepository) UpdateAndReplaceEventsAndBoards(notification *models.NotificationConfiguration) error {
+	if err := r.Update(notification); err != nil {
+		return err
+	}
+
+	if err := r.db.Model(notification).Association("Events").Replace(notification.Events); err != nil {
+		return err
+	}
+
+	if err := r.db.Model(notification).Association("Boards").Replace(notification.Boards); err != nil {
+		return err
+	}
+
+	return nil
 }
