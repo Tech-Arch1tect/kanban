a582106a3540af61641285f38d85375dc04ae959
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-08 20:09:42 +0000

    [client] Add hook to update notification configurations

---
+++ client/src/hooks/notifications/useUpdateNotificationConfiguration.ts
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import { notificationsApi } from "../../lib/api";
+import { toast } from "react-toastify";
+import { NotificationUpdateNotificationRequest } from "../../typescript-fetch-client";
+
+export const useUpdateNotificationConfiguration = () => {
+  const { mutate, error, isError, isSuccess, data, isPending } = useMutation({
+    mutationFn: async (notification: NotificationUpdateNotificationRequest) => {
+      return await notificationsApi.apiV1NotificationsUpdatePost({
+        request: notification,
+      });
+    },
+    onSuccess: () => {
+      toast.success("Notification configuration updated successfully!");
+    },
+    onError: (error: any) => {
+      toast.error(
+        error.message || "Failed to update notification configuration."
+      );
+    },
+  });
+
+  return { mutate, error, isError, isSuccess, data, isPending };
+};
