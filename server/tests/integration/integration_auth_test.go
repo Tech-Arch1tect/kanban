71daf6f04939726007b1e1e606f32e0872193e0f
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-13 22:23:21 +0000

    [server] tests: add simple login tests to ensure the test accounts are working properly

--- server/tests/integration/integration_auth_test.go
+++ server/tests/integration/integration_auth_test.go
@@ -3,6 +3,7 @@ package integration
 import (
 	"log"
 	"net/http"
+	"server/tests"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -32,4 +33,28 @@ func TestRegisterLoginProfile(t *testing.T) {
 
 	require.Equal(t, "integration_user", profile["username"], "username should match")
 	require.Equal(t, "integration@example.com", profile["email"], "email should match")
+}
+
+// this test logs in as a user and confirms the role is not admin
+func TestLoginUser(t *testing.T) {
+	router := getTestingRouter()
+	cookies := Login(t, router, "user")
+
+	response, _ := doAPIRequest(t, router, "GET", "/api/v1/auth/profile", nil, cookies, http.StatusOK)
+
+	require.Equal(t, tests.TestUser.Email, response["email"], "email should match")
+	require.Equal(t, tests.TestUser.Username, response["username"], "username should match")
+	require.Equal(t, "user", response["role"], "role should match")
+}
+
+// this test logs in as an admin and confirms the role is admin
+func TestLoginAdmin(t *testing.T) {
+	router := getTestingRouter()
+	cookies := Login(t, router, "admin")
+
+	response, _ := doAPIRequest(t, router, "GET", "/api/v1/auth/profile", nil, cookies, http.StatusOK)
+
+	require.Equal(t, tests.TestAdminUser.Email, response["email"], "email should match")
+	require.Equal(t, tests.TestAdminUser.Username, response["username"], "username should match")
+	require.Equal(t, "admin", response["role"], "role should match")
 }
