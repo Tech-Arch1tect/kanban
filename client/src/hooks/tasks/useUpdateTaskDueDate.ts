96e50f6f09b8e5de78f829484716cc4bc3a1a6ca
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-02-26 15:45:43 +0000

    [client] add hook to update task due date

---
+++ client/src/hooks/tasks/useUpdateTaskDueDate.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { tasksApi } from "../../lib/api";
+import { toast } from "react-toastify";
+import { TaskUpdateTaskDueDateRequest } from "../../typescript-fetch-client";
+
+export const useUpdateTaskDueDate = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, error, isError, isSuccess, data, isPending } = useMutation({
+    mutationFn: async (task: TaskUpdateTaskDueDateRequest) => {
+      return await tasksApi.apiV1TasksUpdateDueDatePost({
+        request: {
+          taskId: task.taskId,
+          dueDate: task.dueDate,
+        },
+      });
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ["task", response.task?.id],
+      });
+      toast.success("Task due date updated successfully!");
+    },
+    onError: (error: any) => {
+      toast.error(error.message || "Failed to update task due date.");
+    },
+  });
+
+  return { mutate, error, isError, isSuccess, data, isPending };
+};
