cc3384b3578447de558a5aacd6e3581ad7ae401f
Tech-Arch1tect <108552582+Tech-Arch1tect@users.noreply.github.com>
2025-03-06 22:20:39 +0000

    feat(taskColours): Add client hook for updating task colour

---
+++ client/src/hooks/tasks/useUpdateTaskColour.ts
@@ -0,0 +1,36 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { tasksApi } from "../../lib/api";
+import { toast } from "react-toastify";
+import {
+  TaskUpdateTaskColourRequest,
+  TaskUpdateTaskColourResponse,
+} from "../../typescript-fetch-client";
+
+export const useUpdateTaskColour = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate, error, isError, isSuccess, data, isPending } = useMutation({
+    mutationFn: async (task: TaskUpdateTaskColourRequest) => {
+      return await tasksApi.apiV1TasksUpdateColourPost({
+        request: {
+          taskId: task.taskId,
+          colour: task.colour,
+        },
+      });
+    },
+    onSuccess: (response: TaskUpdateTaskColourResponse) => {
+      queryClient.invalidateQueries({ queryKey: ["task", response.task?.id] });
+      if (response.task?.parentTaskId) {
+        queryClient.invalidateQueries({
+          queryKey: ["task", response.task?.parentTaskId],
+        });
+      }
+      toast.success("Task colour updated successfully!");
+    },
+    onError: (error: any) => {
+      toast.error(error.message || "Failed to update task colour.");
+    },
+  });
+
+  return { mutate, error, isError, isSuccess, data, isPending };
+};
